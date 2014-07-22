b16e56d07143fea5a2ead16d665b8fb732547bf6
takuya sato <sato-taku@klab.com>
2014-07-22 19:03:50 +0900

    PHP5.4 build-in web server support

--- app/webroot/index.php
+++ app/webroot/index.php
@@ -2,6 +2,15 @@
 define('ROOT_DIR', dirname(dirname(__DIR__)).'/');
 define('APP_DIR', ROOT_DIR.'/app/');
 
+// PHP5.4 built-in web server support
+if (php_sapi_name() === 'cli-server') {
+    $cli_filepath = preg_replace('/(\?.*)\z/', '', $_SERVER['REQUEST_URI']);
+    if (is_file(__DIR__.$cli_filepath)) {
+        return false;
+    }
+    $_REQUEST['dc_action'] = preg_replace('/\A\//', '', $cli_filepath);
+}
+
 require_once ROOT_DIR.'vendor/dietcake/dietcake/dietcake.php';
 require_once CONFIG_DIR.'bootstrap.php';
 require_once CONFIG_DIR.'core.php';
