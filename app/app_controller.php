3555af41134a0370d9c29044e553c5e75b3054a2
tsukimiya <tsukimiya@gmail.com>
2015-03-12 23:00:03 +0900

    AppControllerにredirectメソッドを追加

--- app/app_controller.php
+++ app/app_controller.php
@@ -3,4 +3,32 @@
 class AppController extends Controller
 {
     public $default_view_class = 'AppLayoutView';
+
+    /**
+     *
+     *
+     * @param $url
+     * @param array $params
+     */
+    public function redirect($url, $params = [])
+    {
+        $query = http_build_query($params);
+        if (strlen($query) > 0) {
+            $query = '?' . $query;
+        }
+        if ($url === '') {
+            $url = substr($_SERVER['REQUEST_URI'], strlen(APP_BASE_PATH));
+            $url = APP_URL . APP_BASE_PATH . urlencode($url);
+        } elseif ($url === '/') {
+            $url = APP_URL . APP_BASE_PATH . $query;
+        } elseif (strpos($url, 'http') === 0) {
+            $url = $url . $query;
+        } else if (strpos($url, '/') === 0) { // $url = '/foo/bar'
+            $url = APP_URL . APP_BASE_PATH . substr($url, 1) . $query;
+        } else { // $url = 'foo/bar'
+            $url = APP_URL . APP_BASE_PATH . $url . $query;
+        }
+        header('Location: ' . $url);
+        exit;
+    }
 }
--- app/config/core.production.php
+++ app/config/core.production.php
@@ -2,7 +2,7 @@
 define('ENV_PRODUCTION', true);
 define('APP_HOST', $_SERVER["HTTP_HOST"]);
 define('APP_BASE_PATH', '/');
-define('APP_URL', 'http://'.$_SERVER["HTTP_HOST"].'/');
+define('APP_URL', 'http://'.$_SERVER["HTTP_HOST"]);
 
 error_reporting(E_ALL | E_STRICT);
 ini_set('display_errors', 'Off');
